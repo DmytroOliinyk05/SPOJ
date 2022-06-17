38a53e701b90dffcbc7c4f9771a66849ef37c218
DOlii05 <92109370+DmytroOliinyk05@users.noreply.github.com>
2022-06-18 00:25:34 +0200

    Add files via upload

---
+++ ANARC09B.cs
@@ -0,0 +1,36 @@
+﻿using System;
+public static class ANARC09B
+{
+    public static long Solve(int tileWidth, int tileHeight)
+    {
+        int squareWidth = tileHeight / GreatestCommonDivisor(tileWidth, tileHeight);
+        int squareHeight = tileWidth / GreatestCommonDivisor(tileWidth, tileHeight);
+        return squareWidth * (long)squareHeight;
+    }
+
+    private static int GreatestCommonDivisor(int a, int b)
+    {
+        int temp;
+        while (b != 0)
+        {
+            temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
+}
+
+public static class Program
+{
+    private static void Main()
+    {
+        int[] line;
+        while ((line = Array.ConvertAll(Console.ReadLine().Split(), int.Parse))[0] != 0)
+        {
+            Console.WriteLine(
+                ANARC09B.Solve(line[0], line[1]));
+        }
+    }
+}
