577c3012db1d1ee0026904ad0f745d9c75d29089
DOlii05 <92109370+DmytroOliinyk05@users.noreply.github.com>
2022-06-18 00:22:35 +0200

    Add files via upload

---
+++ ABSP1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+public static class ABSP1
+{
+    public static long Solve(int[] numbers)
+    {
+        long absoluteDifferencesSum = 0;
+        for (int i = 0; i < numbers.Length; ++i)
+        {
+            int number = numbers[i];
+            int smallerNumbersCount = i;
+            int largerNumbersCount = numbers.Length - 1 - i;
+
+            absoluteDifferencesSum += (smallerNumbersCount - largerNumbersCount) * (long)number;
+        }
+
+        return absoluteDifferencesSum;
+    }
+}
+
+public static class Program
+{
+    private static void Main()
+    {
+        int remainingTestCases = int.Parse(Console.ReadLine());
+        while (remainingTestCases-- > 0)
+        {
+            int numberCount = int.Parse(Console.ReadLine());
+            int[] numbers = Array.ConvertAll(
+                Console.ReadLine().Split(default(char[]), StringSplitOptions.RemoveEmptyEntries),
+                int.Parse);
+
+            Console.WriteLine(
+                ABSP1.Solve(numbers));
+        }
+    }
+}
